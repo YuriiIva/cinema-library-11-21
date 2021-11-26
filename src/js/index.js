017194a3d31da17874b8b7f7532749e184ed85bd
YurkaBest <Ivaschenko_u@ukr.net>
2021-11-26 12:45:21 +0200

    add folders

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import '../sass/main.scss';
+import refs from './refs';
+import refs from './api-services';
+import refs from './modal-card';
+import refs from './modal-team';
+import refs from './render-markup';
