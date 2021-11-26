f5e87af17cffa56cc6501407fee0493b1c97e2c4
YurkaBest <Ivaschenko_u@ukr.net>
2021-11-26 13:10:57 +0200

    fix structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './sass/main.scss';
+
+import './js/api-services';
+import './js/modal-card';
+import './js/modal-team';
+import './js/render-markup';
--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-import '../sass/main.scss';
-import refs from './refs';
-import refs from './api-services';
-import refs from './modal-card';
-import refs from './modal-team';
-import refs from './render-markup';
