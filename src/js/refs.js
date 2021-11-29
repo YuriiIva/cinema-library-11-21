0346cc7dbd9ebf52bb629bdb0168bcadaa2b4be6
MaksVibe <parunovmaksym@gmail.com>
2021-11-29 13:04:19 +0200

    Update switcher

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,7 @@ export default {
   btnFilm: document.querySelector(".btn-hero1"),
   inputFilm: document.querySelector("#hero1-inputr"),
   ulGallery: document.querySelector(".js-gallery"),
+  ulLibrary: document.querySelector(".js-library"),
   modalInfo: document.querySelector(".js-modal-info"),
   backdrop: document.querySelector(".backdrop"),
   closeModalBtn: document.querySelector('[data-action="close"]'),
--- src/js/switch-page.js
+++ src/js/switch-page.js
@@ -4,16 +4,24 @@ function switchToLibrary(e) {
   e.preventDefault();
   refs.homeNav.classList.remove("header__nav-active");
   refs.libNav.classList.add("header__nav-active");
+
   refs.heroHome.classList.add("vusually-hidden");
   refs.heroLib.classList.remove("vusually-hidden");
+
+  refs.ulGallery.classList.add("vusually-hidden");
+  refs.ulLibrary.classList.remove("vusually-hidden");
 }
 
 function switchToHome(e) {
   e.preventDefault();
   refs.libNav.classList.remove("header__nav-active");
   refs.homeNav.classList.add("header__nav-active");
+
   refs.heroLib.classList.add("vusually-hidden");
   refs.heroHome.classList.remove("vusually-hidden");
+
+  refs.ulGallery.classList.remove("vusually-hidden");
+  refs.ulLibrary.classList.add("vusually-hidden");
 }
 
 refs.libNav.addEventListener("click", switchToLibrary);
